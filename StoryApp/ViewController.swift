faebfd9facb2b02708ec65ab55858373f8bd47ae
cyeksan <yeksancansu@gmail.com>
2023-08-13 02:49:40 +0200

    Initial Commit

---
+++ StoryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryApp
+//
+//  Created by Cansu Aktas on 2023-08-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
